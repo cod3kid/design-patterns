c0e2b3c7a1d76c1da3c0eb91dafb5d5b36ad28cd
cod3kid <djsufail@gmail.com>
2021-08-15 00:30:45 +0530

    Iterator Pattern part 2

--- java/behavioural-patterns/iterator/src/BrowseHistory.java
+++ java/behavioural-patterns/iterator/src/BrowseHistory.java
@@ -1,46 +1,73 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class BrowseHistory {
-    public List<String> urls = new ArrayList<>();
+    public String[] urls = new String[10];
+    private int count;
 
     public void push(String url) {
-        urls.add(url);
+        urls[count++] = url;
     }
 
     public String pop() {
-        var lastIndex = urls.size() - 1;
-        var lastUrl = urls.get(lastIndex);
-        urls.remove(lastIndex);
 
-        return lastUrl;
+        return urls[--count];
     }
 
     public Iterator createIterator(){
-        return new ListIterator(this);
+    return new ArrayIterator(this);
     }
 
-    public class ListIterator implements Iterator {
+    public class ArrayIterator implements Iterator {
         private BrowseHistory history;
         private int index;
 
-        public ListIterator(BrowseHistory history) {
+        public ArrayIterator(BrowseHistory history) {
             this.history = history;
         }
 
         @Override
         public boolean hasNext() {
-return (index < history.urls.size());
+            return (index < history.count);
         }
 
         @Override
         public String current() {
-return history.urls.get(index);
+            return history.urls[index];
         }
 
         @Override
         public void next() {
-index++;
+            index++;
         }
+
     }
+
+    // If type of the iterator is List we can use the following code
+    // If we change the type of the iterator only this BrowseHistory class will be affected. 
+
+    // public Iterator createIterator(){
+    // return new ListIterator(this);
+    // }
+
+    // public class ListIterator implements Iterator {
+    // private BrowseHistory history;
+    // private int index;
+
+    // public ListIterator(BrowseHistory history) {
+    // this.history = history;
+    // }
+
+    // @Override
+    // public boolean hasNext() {
+    // return (index < history.urls.size());
+    // }
+
+    // @Override
+    // public String current() {
+    // return history.urls.get(index);
+    // }
+
+    // @Override
+    // public void next() {
+    // index++;
+    // }
+    // }
 }
