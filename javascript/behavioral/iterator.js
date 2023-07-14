1a5ea46f0b8e244547e2fdaf534e90d6b2f4db71
cod3kid <djsufail@gmail.com>
2023-07-14 14:25:34 +0530

    Iterator Pattern

---
+++ javascript/behavioral/iterator.js
@@ -0,0 +1,31 @@
+class Iterator {
+  constructor(items) {
+    this.items = items;
+    this.index = 0;
+  }
+
+  hasNext() {
+    return this.index < this.items.length - 1;
+  }
+
+  current() {
+    return this.items[this.index];
+  }
+
+  next() {
+    this.index += 1;
+    return this.items[this.index];
+  }
+
+  reset() {
+    this.index = 0;
+  }
+}
+
+const arr = ["Indira Nagar", "Domulur", "Koramangala", "Silk Board Junction"];
+const iterator = new Iterator(arr);
+
+console.log(iterator.current());
+while (iterator.hasNext()) {
+  console.log(iterator.next());
+}
